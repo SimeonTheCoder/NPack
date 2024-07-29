698cd86c0ef91d4518478f8109061927d0c66296
Simeon Petkov <petkov.moni@gmail.com>
2024-07-29 20:51:57 +0300

    Support for multiple libraries

--- Main.java
+++ Main.java
@@ -4,8 +4,10 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        if (args[0].equals("--get")) {
+        if (args[0].equals("--get") && args.length > 1) {
             PackageManager.get(args);
+        } else if(args[0].equals("--get")) {
+            PackageManager.get();
         }
     }
 }
--- npack/PackageManager.java
+++ npack/PackageManager.java
@@ -29,10 +29,12 @@ public class PackageManager {
             }
         }
 
-        File theDir = new File(System.getProperty("user.dir") + "/build");
-        if (!theDir.exists()) {
-            theDir.mkdirs();
-        }
+//        if(args[2].equals("--nodir")) {
+            File theDir = new File(System.getProperty("user.dir") + "/build");
+            if (!theDir.exists()) {
+                theDir.mkdirs();
+            }
+//        }
 
         String repo = String.format(
                 "https://raw.githubusercontent.com/%s/%s/main/package.nlb",
@@ -61,6 +63,59 @@ public class PackageManager {
             Runtime.getRuntime().exec(
                     String.format("curl -o build/%s.class %s", line, fileLocation)
             );
+        }
+    }
+
+    public static void get() throws IOException {
+        URL url = new URL("https://raw.githubusercontent.com/SimeonTheCoder/NDepo/main/libs.nlb");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+
+        Scanner libScanner = new Scanner(connection.getInputStream());
+
+        String user = "", repoName = "";
+
+        while(libScanner.hasNextLine()) {
+            String line = libScanner.nextLine();
+
+            String[] tokens = line.split(" ");
+
+            repoName = tokens[1];
+            user = tokens[2];
+
+            File theDir = new File(System.getProperty("user.dir") + "/build");
+            if (!theDir.exists()) {
+                theDir.mkdirs();
+            }
+
+            String repo = String.format(
+                    "https://raw.githubusercontent.com/%s/%s/main/package.nlb",
+                    user,
+                    repoName
+            );
+
+            System.out.println("Downloading package.nlb from: " + repo);
+
+            Runtime.getRuntime().exec("curl -o build/package.nlb " + repo);
+
+            Scanner scanner = new Scanner(new File("build/package.nlb"));
+
+            while (scanner.hasNextLine()) {
+                String line2 = scanner.nextLine();
+
+                String fileLocation = String.format(
+                        "https://raw.githubusercontent.com/%s/%s/main/%s.class",
+                        user,
+                        repoName,
+                        line2
+                );
+
+                System.out.println("Downloading file from: " + fileLocation);
+
+                Runtime.getRuntime().exec(
+                        String.format("curl -o build/%s.class %s", line2, fileLocation)
+                );
+            }
         }
     }
 }
